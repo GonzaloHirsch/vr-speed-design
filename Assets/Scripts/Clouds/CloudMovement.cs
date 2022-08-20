f096fb0f7452ad822a1265568436c545b4d70d6c
Francisco Quesada <fquesada@itba.edu.ar>
2022-08-20 17:12:39 -0300

    add clouds to scene

--- Assets/Scripts/Clouds/CloudMovement.cs
+++ Assets/Scripts/Clouds/CloudMovement.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class CloudMovement : MonoBehaviour
 {
-    public Vector3 direction;
+    public float velocity;
+    private Vector3 startPos;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        velocity = Random.Range(5f, 15F);
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position[0] > 300)
+        {
+            ResetCloud();
+            return;
+        }
+        transform.position += velocity * new Vector3(Time.deltaTime, 0, 0);
+    }
+
+    private void ResetCloud()
+    {
+        velocity = Random.Range(5f, 15F);
+        float newZ = Random.Range(-5f, 5f);
+        if (transform.position[2] + newZ < 150)
+        {
+            newZ = -newZ;
+        }
+        transform.position = startPos + new Vector3(-500, 0, newZ);
     }
 }
