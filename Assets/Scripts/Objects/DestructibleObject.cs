cb7a076e51fc3bbbb4deac281df00a420f901a77
GonzaloHirsch <hirschgonzalo@gmail.com>
2022-08-23 11:47:38 -0300

    Improved balloon popping

--- Assets/Scripts/Objects/DestructibleObject.cs
+++ Assets/Scripts/Objects/DestructibleObject.cs
@@ -8,24 +8,32 @@ public class DestructibleObject : MonoBehaviour
     public float soundTimeout = 0.35f;
     public AudioClip destructionSound;
     private AudioSource audioSource;
+    private bool objectToBeDisabled = false;    // Flag to allow the balloons not to be popped twice
 
-    void Awake () {
+    void Awake()
+    {
         this.audioSource = GetComponent<AudioSource>();
     }
-    
+
     // This method is called when the object is interacted with
     public void OnPointerClick()
     {
-        Score.Instance.AddScore(this.points);   // Add points to score
-        StartCoroutine(this.DisableObject());   // Need to wait until sound is over to disable
-        if (this.audioSource != null && this.destructionSound != null) {
-            this.audioSource.PlayOneShot(this.destructionSound, 1f);
+        if (!this.objectToBeDisabled)
+        {
+            this.objectToBeDisabled = true;
+            Score.Instance.AddScore(this.points);   // Add points to score
+            StartCoroutine(this.DisableObject());   // Need to wait until sound is over to disable
+            if (this.audioSource != null && this.destructionSound != null)
+            {
+                this.audioSource.PlayOneShot(this.destructionSound, 1f);
+            }
         }
     }
 
     private IEnumerator DisableObject()
     {
         yield return new WaitForSeconds(this.soundTimeout);
+        this.objectToBeDisabled = false;
         this.gameObject.SetActive(false);   // Disable the object
     }
 }
