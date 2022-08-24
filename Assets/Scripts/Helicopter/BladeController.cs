336e7e5b2a5b93e0f3b70836f3f69430af5ca4c7
GonzaloHirsch <hirschgonzalo@gmail.com>
2022-08-24 17:01:00 -0300

    Minor improvements to helicopter

--- Assets/Scripts/Helicopter/BladeController.cs
+++ Assets/Scripts/Helicopter/BladeController.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BladeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 10f;
     void Update()
     {
-        transform.Rotate(0, 360 * 2 * Time.deltaTime, 0);
+        transform.Rotate(0, this.speed * Time.deltaTime, 0);
     }
 }
--- Assets/Scripts/Helicopter/Helicopter.cs
+++ Assets/Scripts/Helicopter/Helicopter.cs
@@ -1,107 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Helicopter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private List<Vector3> _positions;
-    private List<Vector3> _angles;
-    private int _currentTarget;
-    private bool _isRotating = false;
-    private Vector3 _initialAngle;
-    private Vector3 _prevAngle;
+    public Vector3[] positions;
+    private int currentPosition = 0;
+    public float speed = 10f;
+    public float positionTolerance = 0.5f;
+    private Vector3 newTargetPosition = Vector3.zero;
 
-    void Start()
-    {
-        Vector3 initialPos = transform.position;
-
-        _prevAngle = transform.rotation.eulerAngles;
-
-        List<Vector3> points = new List<Vector3>
-        {
-            new Vector3(0, 10, 0),
-            new Vector3(-150, -30, 0),
-            new Vector3(0, -10, 150),
-            new Vector3(110, -100, 0),
-            new Vector3(40, 170, 0),
-            new Vector3(0, 0, -150),
-        };
-
-        _positions = new List<Vector3>();
-        _angles = new List<Vector3>
-        {
-            new Vector3(0,0, 0),
-            new Vector3(0,0, 0),
-            new Vector3(0, 90,0),
-            new Vector3(0, 90, 0),
-            new Vector3(0, 0, 0),
-            new Vector3(0, 90, 0),
-            new Vector3(0,90,0)
-        };
-
-        Vector3 prevPoint = initialPos;
-
-        for (int i = 0; i < points.Count; i++)
-        {
-            _positions.Add(prevPoint + points[i]);
-            prevPoint = _positions[i];
-        }
-
-        _positions.Add(initialPos);
-
-        _currentTarget = 0;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (_isRotating)
+        // Update the position, move to the target
+        this.transform.position = Vector3.MoveTowards(this.transform.position, this.positions[this.currentPosition], Time.deltaTime * this.speed);
+        // Near enough from the position, move to next one
+        if (Vector3.Distance(this.transform.position, this.positions[this.currentPosition]) <= this.positionTolerance)
         {
-            moveToNextAngle();
-        }else
-        {
-            moveToNextPosition();
+            this.currentPosition = (this.currentPosition + 1) % this.positions.Length;
+            // Look at the target
+            this.newTargetPosition = this.positions[this.currentPosition];
+            // Fix the y position to avoid rotations in x
+            this.newTargetPosition.y = this.transform.position.y;
+            this.transform.LookAt(newTargetPosition);
         }
     }
 
-    private void addPosition(Vector3 position)
+    void OnDrawGizmos()
     {
-        
-    }
-
-    private void moveToNextPosition()
-    {
-        var step = 25 * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, _positions[_currentTarget], step);
-        if (Vector3.Distance(transform.position, _positions[_currentTarget]) < 0.001f)
+        Gizmos.color = Color.red;
+        if (this.positions != null)
         {
-            _currentTarget = (_currentTarget + 1) % _positions.Count;
-            _isRotating = true;
+            foreach (Vector3 v in this.positions)
+            {
+                Gizmos.DrawSphere(v, 2f);
+            }
         }
-    }
-    private void moveToNextAngle()
-    {
-
-        transform.Rotate(0.5f*Time.deltaTime * _angles[_currentTarget]);
-        float x = transform.rotation.eulerAngles[0];
-        float y = transform.rotation.eulerAngles[1];
-        float z = transform.rotation.eulerAngles[2];
-        x = x > 180 ? (x - 360) : x;
-        y = y > 180 ? (y - 360) : y;
-        z = z > 180 ? (z - 360) : z;
-        Vector3 newAngle = new Vector3(x, y, z);
-        Vector3 targetAngle =_prevAngle + _angles[_currentTarget];
-        targetAngle.x = targetAngle.x > 180 ? (targetAngle.x - 360) : targetAngle.x;
-        targetAngle.y = targetAngle.y > 180 ? (targetAngle.y - 360) : targetAngle.y;
-        targetAngle.z = targetAngle.z > 180 ? (targetAngle.z - 360) : targetAngle.z;
-        Debug.Log(newAngle);
-        if (Vector3.Distance(newAngle, targetAngle)< 1f)
-        {
-            _isRotating = false;
-            _prevAngle = newAngle;
-        }
-
-
     }
 }
